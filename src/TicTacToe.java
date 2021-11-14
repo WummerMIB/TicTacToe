990900c9deaf7b99ed1c19d92c55faea861b3bcb
WummerMIB <mohr-marco@gmx.de>
2021-11-14 02:42:35 +0100

    comments and a small change in functionScanner

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -23,14 +23,16 @@ public class TicTacToe {
 	public static String con = null;
 	// Variable for player that starts the game
 	public static String player = "Player 1";
-	
+
 	public static void main(String[] args) {
-		
+
 		// Variable that changes space in array to X for Player 1 and to O for Player 2
 		char changeSymbol = ' ';
-		
-		//while loop if user wants to continue
+
+		// while loop if user wants to continue
 		while (continueGame == true) {
+			// Shows in Console what Game is played
+			System.out.println("TicTacToe Spiel");
 			// Asks Player if he wants to play against a Person or the Computer
 			System.out.println("1 Zweispielermodus");
 			System.out.println("2 CPU");
@@ -43,21 +45,31 @@ public class TicTacToe {
 				// Calls Method to show the Gameboard and explain the game
 				explainGame(gameboard);
 				System.out.println("Sie haben Zweispieler Modus Ausgewählt ");
-				// loops as long as no player wins if one fulfills winCon than break loop and give winner message
+				// loops as long as no player wins if one fulfills winCon than break loop and
+				// give winner message
 				while (winCon == false) {
 					// if variable player is Player 1 than go on if condition
 					if (player == "Player 1") {
 						System.out.println("Spieler 1 ist am Zug");
-						// As long as user  
+						// As long as the user does not place the symbol in the right place its loops
 						while (rightPlace == false) {
+							// calls Method fuctionScanner for Input from player to place a symbol
 							functionScanner();
+							// Variable that shows which symbol is placed
 							changeSymbol = 'X';
+							// Calls Method rightPlacement to check if the Player but the symbol on the
+							// right place
 							rightPlacement(gameboard, changeSymbol, change, player);
 						}
+						// changes Variable to false for player 2
 						rightPlace = false;
+						// Calls Method to check if the player won the game
 						winCondition(gameboard, player);
+						// changes String to Player 2 to change the player
 						player = "Player 2";
-					} else if (player == "Player 2") {
+					}
+					// now player is Player 2 and it is Player 2 turn
+					else if (player == "Player 2") {
 						while (rightPlace == false) {
 							functionScanner();
 							changeSymbol = 'O';
@@ -65,6 +77,7 @@ public class TicTacToe {
 						}
 						rightPlace = false;
 						winCondition(gameboard, player);
+						// changes String to Player 1 to go back to player 1
 						player = "Player 1";
 					}
 				}
@@ -81,11 +94,14 @@ public class TicTacToe {
 						}
 						rightPlace = false;
 						winCondition(gameboard, player);
+						// changes String to CPU to let the computer play
 						player = "CPU";
-					} else if (player == "CPU") {
+					}
+					// Player is now CPU meaning the Computer plays
+					else if (player == "CPU") {
 						while (rightPlace == false) {
+							// calls Method to generate a Random Number
 							randomNumber();
-							System.out.println(change);
 							changeSymbol = 'O';
 							rightPlacement(gameboard, changeSymbol, change, player);
 						}
@@ -97,7 +113,7 @@ public class TicTacToe {
 			} else {
 				System.out.println("Bitte geben sie 1 oder 2 ein");
 			}
-
+			// calls Method to repeat the game
 			continueGame();
 
 		}
@@ -106,35 +122,53 @@ public class TicTacToe {
 	// Method to Print out the TicTacToe field
 	public static void printTicTacToeBoard(char[] gameboard) {
 
+		// Prints out the board and puts in the gameboard[x] and show the value in the
+		// array on that place x
 		System.out.println("[" + gameboard[0] + "][" + gameboard[1] + "][" + gameboard[2] + "]");
 		System.out.println("[" + gameboard[3] + "][" + gameboard[4] + "][" + gameboard[5] + "]");
 		System.out.println("[" + gameboard[6] + "][" + gameboard[7] + "][" + gameboard[8] + "]");
 	}
 
-	// Method for an Input for the User to place an X or O
+	// Method for Users to Input numbers
 	public static int functionScanner() {
+		// Variable for loop to look if user makes the right Input
 		boolean rightInput = false;
+		// while loop to continue if user makes wrong input
 		while (rightInput == false) {
+			// Scanner for number Input from User
 			Scanner sc = new Scanner(System.in);
-
+			// look if Input from User is a number
 			if (sc.hasNextInt()) {
+				// sets Variable change to number from User Input
 				change = sc.nextInt();
-				if (change <= 9) {
+				// if number is smaller than 9 and bigger than 0 than return number from User
+				// Input
+				if (change <= 9 && !(change <= 0)) {
 					return change;
-				} else {
-					System.out.println("Bitte geben sie ein Zahl eine Richtige Zahl ein");
 				}
-
-			} else {
+				// if number is bigger than 9 or smaller or equal to 0 Print this message
+				else {
+					System.out.println("Bitte geben sie ein Zahl eine Zahl zwischen 1 und 9 ein");
+				}
+			}
+			// if Input is a double number then print this message
+			else if (sc.hasNextDouble()) {
+				System.out.println("Bitte geben sie keine Kommazahl ein");
+			}
+			// if Input is a latter than Print this message
+			else {
 				System.out.println("Bitte geben sie eine Zahl ein");
 			}
 
 		}
+		// method needs to return an int number and if it return it in if condition than
+		// you get an error
 		return change;
 	}
 
 	// Method that gives Random Number between one and nine
 	public static int randomNumber() {
+		// generates number between 0 and 8 and + 1 because Zero can't be an Input
 		Random randNum = new Random();
 		change = randNum.nextInt(9) + 1;
 		return change;
@@ -143,6 +177,8 @@ public class TicTacToe {
 	// Method to check if the Player or the CPU has won and changes winCon to true
 	public static boolean winCondition(char[] gameboard, String player) {
 
+		// check in array on point 0, 1 and 2 if there is an X or O and returns winCone
+		// true if true
 		if (gameboard[0] == 'X' && gameboard[1] == 'X' && gameboard[2] == 'X'
 				|| gameboard[0] == 'O' && gameboard[1] == 'O' && gameboard[2] == 'O') {
 
@@ -185,13 +221,15 @@ public class TicTacToe {
 			System.out.println("Sieger " + player);
 			return winCon = true;
 		}
+		// if the array is full and no player won then Print that the game is a draw
 		if (gameboard[0] != ' ' && gameboard[1] != ' ' && gameboard[2] != ' ' && gameboard[3] != ' '
 				&& gameboard[4] != ' ' && gameboard[5] != ' ' && gameboard[6] != ' ' && gameboard[7] != ' '
 				&& gameboard[8] != ' ') {
 			System.out.println("Unentschieden");
 			return winCon = true;
 		}
-		return false;
+		// if no one has won the game return false and continue the game;
+		return winCon = false;
 	}
 
 	// Method to place an X or an O
@@ -234,6 +272,8 @@ public class TicTacToe {
 	// Method that looks if the Player can put an X or O in the field and don't
 	// overwrite a Player Symbol
 	public static boolean rightPlacement(char[] gameboard, char changeSymbol, int change, String player) {
+		// looks in array on the place from User Input if there is an X or O than return
+		// that you can't place the symbol there
 		if (gameboard[change - 1] == 'X' || gameboard[change - 1] == 'O') {
 			if (player == "CPU") {
 				System.out.println("Thinking...");
@@ -241,13 +281,18 @@ public class TicTacToe {
 			System.out.println("Bitte setzen sie ihr Symbol wo noch kein Symbol ist");
 			return rightPlace = false;
 		}
+		// if array is on user Input -1 empty than Print the Board and return rightPlace
+		// true
 		if (gameboard[change - 1] == ' ') {
 			butInBoardXOrOAndPrintBoard(gameboard, changeSymbol);
 			return rightPlace = true;
 		}
+		// method needs to return an boolean and if it return it in if condition than
+		// you get an error
 		return false;
 	}
 
+	// Method to explain the game only Prints Board and explain how to play
 	public static void explainGame(char[] gameboard) {
 		System.out.println("[1][2][3]");
 		System.out.println("[4][5][6]");
@@ -257,31 +302,45 @@ public class TicTacToe {
 
 	}
 
+	// Method to start a new game
 	public static void continueGame() {
+		// boolean to check if the right Input was made
 		boolean rigthContinueInput = false;
-		while(rigthContinueInput == false) {
-		System.out.println("Möchten Sie nocheinmal Spielen? (ja/nein)");
-		Scanner continueGameScanner = new Scanner(System.in);
-		con = continueGameScanner.nextLine();
-
-		if (con.equals("nein")) {
-			System.out.println("Danke fürs Spielen!");
-			continueGame = false;
-			rigthContinueInput = true;
-		} else if (con.equals("ja")) {
-			System.out.println("Das Spiel startet von vorne!");
-			winCon = false;
-			for (int i = 0; i <= 8; i++) {
-				gameboard[i] = ' ';
+		// loops until the right Input ja or nein
+		while (rigthContinueInput == false) {
+			System.out.println("Möchten Sie nocheinmal Spielen? (ja/nein)");
+			// Scanner for User Input that save it in the String con
+			Scanner continueGameScanner = new Scanner(System.in);
+			con = continueGameScanner.nextLine();
+			// if Input is nein than dont't continue the game
+			if (con.equals("nein")) {
+				System.out.println("Danke fürs Spielen!");
+				continueGame = false;
+				// variable that shows that Input is true
+				rigthContinueInput = true;
 			}
-			rigthContinueInput = true;
-			player = "Player 1";
-			continueGame = true;
-		} else if (!con.equals("nein") && !con.equals("ja")) {
-			System.out.println("Bitte geben sie ja oder nein ein");
-			rigthContinueInput = false;
+			// if Input is ja than continue the game
+			else if (con.equals("ja")) {
+				System.out.println("Das Spiel startet von vorne!");
+				// winCon to false for next game
+				winCon = false;
+				// array gets reset for next game all values go back to ' '
+				for (int i = 0; i <= 8; i++) {
+					gameboard[i] = ' ';
+				}
+				// variable that shows that Input is true
+				rigthContinueInput = true;
+				// changes player to Player 1 that in a new game Player 1 starts
+				player = "Player 1";
+				continueGame = true;
+			}
+			// if Input is not ja or nein then give out an error message and repeat the
+			// question if User wants to continue
+			else if (!con.equals("nein") && !con.equals("ja")) {
+				System.out.println("Bitte geben sie ja oder nein ein");
+				rigthContinueInput = false;
+			}
 		}
-	}
 	}
 
 }
